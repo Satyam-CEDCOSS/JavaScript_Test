e6c61f6e536fb4e69a4b5f62ac703de315e18ded
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-22 18:34:42 +0530

    Test Submission

---
+++ src/JS/script.js
@@ -0,0 +1,13 @@
+var regex  = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/);
+function checking(){
+    let password = document.getElementById("input_password").value;
+    console.log(password)
+    if (regex.test(password)){
+        document.getElementById("display").innerHTML = "Valid Password";
+        document.getElementById("display").style.color = "green"
+    }
+    else{
+        document.getElementById("display").innerHTML = "Invalid Password";
+        document.getElementById("display").style.color = "red"
+    }
+}
