900fda6e1bed703eeb75653ae41f2b001d5b53a2
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-22 18:39:20 +0530

    Add Comment

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,3 +1,4 @@
+// Main Function 
 var regex  = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/);
 function checking(){
     let password = document.getElementById("input_password").value;
