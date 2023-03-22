57a42c16823336f4cc5ea17e1cdd9c7cc7d70bbb
satyam01032023 <satyambajpai@cedcoss.com>
2023-03-22 18:49:39 +0530

    removing log

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,7 +2,6 @@
 var regex  = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/);
 function checking(){
     let password = document.getElementById("input_password").value;
-    console.log(password)
     if (regex.test(password)){
         document.getElementById("display").innerHTML = "Valid Password";
         document.getElementById("display").style.color = "green"
